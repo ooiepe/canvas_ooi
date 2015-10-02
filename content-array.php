46a97ed5d7eb9e227e9ab6d7b3c78a1a50367b06
Sage Lichtenwalner <sage@marine.rutgers.edu>
2015-10-02 10:24:10 -0400

    Updates to the Array template

--- content-array.php
+++ content-array.php
@@ -3,10 +3,10 @@
  * Array Content Template
  *
  */
-
 $title_before = '<h1 class="title entry-title">';
 $title_after = '</h1>';
 
+// Setup Header Styles
 if ( ! is_single() ) {
   $title_before = '<h2 class="title entry-title">';
   $title_after = '</h2>';
@@ -16,6 +16,7 @@ if ( ! is_single() ) {
 
 $page_link_args = apply_filters( 'woothemes_pagelinks_args', array( 'before' => '<div class="page-link">' . __( 'Pages:', 'woothemes' ), 'after' => '</div>' ) );
 
+// Start Generating Content
 woo_post_before();
 ?>
 <article <?php post_class(); ?>>
@@ -25,39 +26,92 @@ woo_post_before();
   </header>
   <?php //woo_post_meta(); ?>
 	<section class="entry">
-    <?php
-      if ( ! is_singular () ) {
-        the_excerpt();
-      } else {
-        the_content( __( 'Continue Reading &rarr;', 'woothemes' ) );      
-        $pod = pods('array',get_the_ID());
-      ?>
-      <div>Depth: <?php echo get_post_meta( get_the_ID(), 'depth', true); ?></div>
-      <div>Latitude: <?php echo get_post_meta( get_the_ID(), 'latitude', true); ?></div>
-      <div>Longitude: <?php echo get_post_meta( get_the_ID(), 'longitude', true); ?></div>
-      <div>Setting: <?php echo get_post_meta( get_the_ID(), 'setting', true); ?></div>
-      <div>Map: <?php echo get_post_meta( get_the_ID(), 'array_map', true); ?></div>
+  <?php
+    if ( ! is_singular () ) {
+      // Listing
+      the_excerpt();
+    } else { 
+      // Single Entry
+      $pod = pods('array',get_the_ID());
+    ?>
+    <div class="fourcol-three">
+      <?php the_content( __( 'Continue Reading &rarr;', 'woothemes' ) );    ?> 
+      
+      <h3>Photos</h3>
+      <?php  
+        $photos = get_post_meta( get_the_ID(), 'photos', false);
+
+        if ( ! empty( $photos ) ) {
+          foreach ( $photos as $photo ): ?>
+          <div class="fivecol-one">
+            <?php echo the_attachment_link($photo['ID'],false,'',true);?>
+          </div>
+          <?php endforeach; ?>
+          <div class="clear"></div>
+          <?php
+        } else {
+          echo 'No photos';    
+        }
+      ?>       
+      <h3>Sites</h3>
+      <p>This array includes following research sites and platforms.</p>
       <?php
-        $params = array( 'orderby'=>'site_name ASC', 'limit'=>-1); 
+        $params = array( 'orderby'=>'post_title ASC', 'limit'=>-1); 
         $sites = $pod->field('sites',$params);
         if ( ! empty( $sites ) ) {
         ?>
-      <h3>Sites</h3>
       <table>
-        <tr><th>Code</th><th>Name</th><th>Depth</th></tr>
+        <tr><th>Site Name</th><th>Water Depth</th></tr>
           <?php foreach ( $sites as $site ): ?>
         <tr>
-          <td><?php echo get_the_title($site['ID']);?></td>
-          <td><?php echo sprintf( '<a href="%s">%s</a>', esc_url(get_permalink($site['ID'])), get_post_meta($site['ID'],'site_name',true) );?></td>
-          <td><?php print_r( get_post_meta($site['ID'],'depth',true) );?></td>
+          <td><?php echo sprintf( '<a href="%s">%s</a>', esc_url(get_permalink($site['ID'])), get_post_meta($site['ID'],'site_name',true) );?>
+          (<?php echo get_the_title($site['ID']);?>)</td>
+          <td><?php 
+            $depth = get_post_meta($site['ID'],'depth',true);
+            if ($depth) echo $depth;?></td>
         </tr>
           <?php endforeach; ?>
       </table>
+      <?php } //endif empty ?>        
+    </div>
+    <div class="fourcol-one last">
+      <div><strong>Array Map</strong>
+        <?php  echo sprintf( '<a href="%s">%s</a>', 
+          get_post_meta( get_the_ID(), 'array_map._src.full', true), 
+          get_post_meta( get_the_ID(), 'array_map._img.medium', true) );
+        ?></div>
+      <div><p><strong>Approximate Water Depth</strong><br>
+        <?php echo get_post_meta( get_the_ID(), 'depth', true); ?></p></div>
+      <div><p><strong>Central Mooring Location</strong><br>
+        <?php echo get_post_meta( get_the_ID(), 'latitude', true); ?>, 
+        <?php echo get_post_meta( get_the_ID(), 'longitude', true); ?></p></div>
+      <div><p><strong>Research Setting</strong><br>
+        <?php echo get_post_meta( get_the_ID(), 'setting', true); ?></p></div>
+      <div><strong>Research Themes</strong>
+        <ul>
+        <?php 
+        $themes = $pod->field('science_themes');
+        if ( ! empty( $themes ) ) {
+          foreach ( $themes as $theme ) {
+            echo sprintf( '<li><a href="%s">%s</a></li>', esc_url(get_permalink($theme['ID'])), $theme['post_title'] );
+          }
+        } 
+        ?>
+        </ul>
+      </div>
+    </div>
+    <div class="clear"></div>
+    
+    
+    <pre>
+    <div>Map: <?php print_r(get_post_meta( get_the_ID(), 'array_map', false)); ?></div>
+    <div>Photos: <?php print_r(get_post_meta( get_the_ID(), 'photos', false)); ?></div>
+
+
       
-        <?php } //endif empty ?>
-      <?php } //endif is_singular else
-      //wp_link_pages( $page_link_args );      
-    ?>
+    <?php } //endif is_singular else
+    //wp_link_pages( $page_link_args );      
+  ?>
 	</section><!-- /.entry -->
 	<div class="fix"></div>
 <?php
