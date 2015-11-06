b8d543a33386dafdb5aade11b9f41121fef1d30b
Sage Lichtenwalner <sage@marine.rutgers.edu>
2015-11-06 16:37:49 -0500

    Removed comments on media attachment pages

--- functions.php
+++ functions.php
@@ -77,4 +77,13 @@ if (!function_exists('ooi_image_caption')) {
 }
 
 
+function filter_media_comment_status( $open, $post_id ) {
+	$post = get_post( $post_id );
+	if( $post->post_type == 'attachment' ) {
+		return false;
+	}
+	return $open;
+}
+add_filter( 'comments_open', 'filter_media_comment_status', 10 , 2 );
+
 ?>
